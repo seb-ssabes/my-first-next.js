9128fbb255405f4eb4636de3486f8800f84c24d7
Sebastian Olivares <olivares.heredia@gmail.com>
2025-09-20 11:29:04 +0200

    navbar layout done

---
+++ src/app/about/mission/page.jsx
@@ -0,0 +1,5 @@
+export default function Mission() {
+  return (
+    <div>Mission</div>
+  )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,36 @@
-
 import "./globals.css";
+/* eslint-disable @next/next/no-img-element */
+
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   return (
     <html lang="en">
       <body>
+        <header className="w-full bg-white">
+          <nav className="flex justify-between px-6 py-4">
+            <div className="relative">
+              <img
+                src="/logo.png"
+                alt="PrintForge Logo"
+                className="w-[200px] h-auto hidden md:block"
+              />
+              <img
+                src="/icon.png"
+                alt="PrintForge Logo"
+                className="w-[40px] h-auto block md:hidden"
+              />
+            </div>
+              <ul className="flex items-center gap-2.5">
+                <li>3D MODELS</li>
+                <li>ABOUT</li>
+              </ul>
+          </nav>
+        </header>
         {children}
       </body>
     </html>
