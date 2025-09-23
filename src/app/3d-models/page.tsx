87f59e0ef3cc3807855e6bc3b65181bc67641c99
Sebastian Olivares <olivares.heredia@gmail.com>
2025-09-23 11:25:39 +0200

    added category navbar to models page

--- src/app/3d-models/page.tsx
+++ src/app/3d-models/page.tsx
@@ -1,7 +1,7 @@
 import { getAllModels } from "@/app/lib/models"
 import ModelsGrid from "../components/ModelsGrid"
 
-export default async function ModelsPage() {
+export default async function ModelsPage({children}) {
   const models = await getAllModels()
   return (
     <ModelsGrid title="3D Models" models={models} />
